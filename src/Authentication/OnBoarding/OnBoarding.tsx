c43cd62bde9359327c6dd5efb39376754c60233f
Abe <abhi.rawat456@gmail.com>
2020-12-02 23:39:33 +0530

    Aligned Text

--- src/Authentication/OnBoarding/OnBoarding.tsx
+++ src/Authentication/OnBoarding/OnBoarding.tsx
@@ -1,10 +1,51 @@
 import React from "react";
-import { View } from "react-native";
+import { Dimensions, StyleSheet, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+
+import Slide, { SLIDE_HEIGHT } from "./Slide";
+
+const { width, height } = Dimensions.get("window");
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "white" },
+  slider: {
+    height: SLIDE_HEIGHT,
+    backgroundColor: "cyan",
+    borderBottomRightRadius: 75,
+  },
+  footer: {
+    flex: 1,
+  },
+});
 
 interface OnBoardingProps {}
 
 const OnBoarding = () => {
-  return <View style={{ flex: 1, backgroundColor: "cyan" }} />;
+  return (
+    <View style={styles.container}>
+      <View style={styles.slider}>
+        <ScrollView
+          horizontal
+          snapToInterval={width}
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator={false}
+          bounces={false}
+        >
+          <Slide label="Relaxed" />
+          <Slide label="Playful" right />
+          <Slide label="Excentric" />
+          <Slide label="Funky" right />
+        </ScrollView>
+      </View>
+      <View style={styles.footer}>
+        <View
+          style={{ ...StyleSheet.absoluteFillObject, backgroundColor: "cyan" }}
+        />
+        <View
+          style={{ flex: 1, backgroundColor: "white", borderTopLeftRadius: 75 }}
+        ></View>
+      </View>
+    </View>
+  );
 };
 
 export default OnBoarding;
