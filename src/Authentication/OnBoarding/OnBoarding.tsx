31fd16dff09dce0374fec276750d05b18bc7dcef
Abe <abhi.rawat456@gmail.com>
2020-12-02 23:13:06 +0530

    Project Setup

---
+++ src/Authentication/OnBoarding/OnBoarding.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+interface OnBoardingProps {}
+
+const OnBoarding = () => {
+  return <View style={{ flex: 1, backgroundColor: "cyan" }} />;
+};
+
+export default OnBoarding;
